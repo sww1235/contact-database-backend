b5d0802c5e044969733aba7b72e373828da4c93f
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2016-06-07 14:26:36 -0600

    add address struct

--- contact_database.go
+++ contact_database.go
@@ -1,6 +1,16 @@
 package main
 
-
+//address structs are meant for storing the address of an individual or orginization
+type address struct {
+  department string //Optional, used for institutions/campuses
+  institution string //Optional, this is an orginization that the individual is affilated with such as their place of work
+  streetAddress string //Normally will be the company address if institution is filled in unless the personal relationship is more important
+  town string // 
+  province string
+  postalCode string
+  country string
+  
+}
 func main {
 
 }
