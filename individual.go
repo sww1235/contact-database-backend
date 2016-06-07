6912deab8a7673984a9265680bccf6ebef902578
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2016-06-07 14:24:07 -0600

    remove address info from individual struct and lowercase name
    move it to its own struct

--- individual.go
+++ individual.go
@@ -1,20 +1,13 @@
 package main
 
 //Individual is a struct that represents one individual person. 
-//They can be associated with an orginization or not
-type Individual struct {
-  name string //no separation between first and last names to avoid 'issues' with international names, Optional if Institution flag is set
-  department string //Optional, used for institutions/campuses
-  institution string //Optional, this is an orginization that the individual is affilated with such as their place of work
-  streetAddress string //Normally will be the company address if institution is filled in unless the personal relationship is more important
-  town string // 
-  province string
-  postalCode string
-  country string
+//They can be associated with an orginization and/or family or neither.
+type individual struct {
+  add
   
-  family string
+  family string //Optional
   
-  
+  phoneNumbers []string
   emails []string
   
   
