0c13cda53bd7af350a1ab37d3aed64b7112d61c9
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2016-06-07 14:36:18 -0600

    individual struct finally somewhat sane

--- individual.go
+++ individual.go
@@ -3,10 +3,12 @@ package main
 //Individual is a struct that represents one individual person. 
 //They can be associated with an orginization and/or family or neither.
 type individual struct {
-  add
-  
+  name string //no separation between first and last names to avoid 'issues' with international names, Optional if Institution flag is set
+  department string //Optional, used for institutions/campuses
+  institution string //Optional, this is an orginization that the individual is affilated with such as their place of work
   family string //Optional
   
+  addresses []address 
   phoneNumbers []string
   emails []string
   
