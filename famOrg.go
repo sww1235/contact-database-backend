d81ee95609f1bfedf13b33a97542afe2fbeffeca
Stephen Walker-Weinshenker <sww1235@users.noreply.github.com>
2016-06-07 14:50:46 -0600

    Update and rename family.go to famOrg.go

---
+++ famOrg.go
@@ -0,0 +1,15 @@
+package main
+
+type family struct{
+  orginization //family is an orginization. 'inheritance'
+  
+  
+}
+
+type orginization struct{
+  mainAddress address //the main mailing address for the orginization, Optional
+  mainEmail string //main email address for the orginization, Optional
+  
+  members []individual //slice of members
+  
+}
